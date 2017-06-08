dfc8abf34bad9c5705c7a8f8fcfa5e59ffd73ac6
agleverone <alexgreglev@gmail.com>
2017-06-07 22:03:25 -0400

    Pushing moveplayer script

--- Assets/MovePlayer.cs
+++ Assets/MovePlayer.cs
@@ -4,6 +4,53 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour {
 
+    public Transform position1;
+    public Transform position2;
+    public float fadeWaitTime = 2f;
+    private int PlayerState = 0;
+    public AudioClip movementAudio;
+    public AudioSource aSource;
+    private StateMachineBehaviour stateMacBehavoir;
+    private void Awake()
+    {
+        aSource = GetComponent<AudioSource>();
+        
+    }
+
+    public void DoorUp()
+    {
+        //start courotine
+    }
+
+    IEnumerator QuickFadeWait()
+    {
+        //Call fade Steam script
+        yield return new WaitForSeconds(fadeWaitTime);
+        aSource.PlayOneShot(movementAudio);
+        switch (PlayerState)
+        {
+            case 0:
+                {
+                    transform.position = position1.position;
+                    break;
+                }
+            case 1:
+                {
+                    transform.position = position2.position;
+                    break;
+                }
+        }
+        yield return new WaitForSeconds(fadeWaitTime);
+        //Call fade Steam script
+        PlayerState++;
+
+    }
+
+    public void MoveToWater()
+    {
+        //start coroutine
+    }
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +60,6 @@ public class MovePlayer : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    
 }
