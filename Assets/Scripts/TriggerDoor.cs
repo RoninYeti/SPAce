4185801b6f9defcb7348264c249838e38bf4ed3d
RoninYeti <RoninYetcen@live.com>
2017-06-09 07:15:59 -0400

    I added The Perfume "animation" and Jelly animation

---
+++ Assets/Scripts/TriggerDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour {
+        
+    public Animator DoorAnim;
+    
+    
+	void Start()
+    {
+
+    }
+	
+	
+	void Update () {
+        
+	}
+
+    public void OpenDoor()
+    {
+        DoorAnim.SetTrigger("Door Trigger");
+    }
+
+    
+        
+}
---
+++ Assets/Scripts/TriggerRope.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TriggerRope : MonoBehaviour
+{
+
+
+    public float gazeTime = 2f;
+
+    private float timer;
+
+    private bool gazedAt;
+
+    public int pullCount = 0;
+        
+    public Animator RopeAnim;
+
+    [SerializeField]
+    private TriggerDoor opener;
+    
+    void Start()
+    {
+        
+    }
+
+    public void OpenSesame() //this was performthrow
+    {
+        RopeAnim.SetTrigger("RopePull Trigger");
+        opener.OpenDoor();
+    }
+
+    
+    void Update()
+    {
+        if (gazedAt)
+        {
+            timer += Time.deltaTime;
+        //    Debug.Log(timer);
+            
+        }
+    }
+
+    public void PointerEnter()
+    {
+        //Debug.Log("Pointer Enter");
+        gazedAt = true;
+        if (pullCount == 0)
+        {
+            OpenSesame();
+
+            pullCount += 1;
+        }
+    }
+
+    public void PointerExit()
+    {
+        //Debug.Log("Pointer Exit");
+        gazedAt = false;
+    }
+
+    public void PointerDown()
+    {
+     //   Debug.Log("Pointer Down");
+    }
+
+}
