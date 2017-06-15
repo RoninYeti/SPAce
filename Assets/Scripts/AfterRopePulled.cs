8953a86997ade05c0ccc9ef0210e7f68e9f0865a
agleverone <alexgreglev@gmail.com>
2017-06-15 01:17:55 -0400

    found and commented out last debug log

--- Assets/Scripts/AfterRopePulled.cs
+++ Assets/Scripts/AfterRopePulled.cs
@@ -9,7 +9,7 @@ public class AfterRopePulled : StateMachineBehaviour {
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        Debug.Log("This is actually working");
+        //Debug.Log("This is actually working");
 	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
